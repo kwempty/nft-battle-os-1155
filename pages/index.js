597409093bc9401067af24ed0e79a959c84ed0ae
Alejandro Cremades <ale@alecrem.com>
2022-08-04 15:29:31 +0900

    Connect wallet

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Box, Container, Heading } from "@chakra-ui/react";
+import { useAccount } from "wagmi";
+import { Account, Connect } from "../components";
+import { useMounted } from "../hooks";
 
 export default function Home() {
+  const isMounted = useMounted();
+  const { isConnected } = useAccount();
+
   return (
     <Box>
       <Container maxW="container.sm" mt="4em">
         <Heading as="h1" size="4xl">
           NFT Battle
         </Heading>
+        <Box>
+          {isMounted && <Connect />}
+
+          {isMounted && isConnected && (
+            <>
+              <Account />
+            </>
+          )}
+        </Box>
       </Container>
     </Box>
   );
