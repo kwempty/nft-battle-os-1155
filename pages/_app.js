6429ac8f83121defe505b576a3ae66a1759e72fb
Alejandro Cremades <ale@alecrem.com>
2022-08-18 12:05:31 +0900

    Fixed chain

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import {
-  defaultChains,
-  WagmiConfig,
-  createClient,
-  configureChains,
-} from "wagmi";
+import { chain, WagmiConfig, createClient, configureChains } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 
 const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
-const { chains, provider } = configureChains(defaultChains, [
-  alchemyProvider({ alchemyId }),
-]);
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon],
+  [alchemyProvider({ alchemyId })]
+);
 const client = createClient({
   autoConnect: true,
   connectors: [new InjectedConnector({ chains })],
