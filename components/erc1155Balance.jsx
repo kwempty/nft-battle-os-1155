4fae4b6678adc35ac22264cdc76f3392a157a7ed
Alejandro Cremades <ale@alecrem.com>
2022-08-18 15:23:44 +0900

    Support and visual feedback for owning zero or more tokens

--- components/erc1155Balance.jsx
+++ components/erc1155Balance.jsx
@@ -2,7 +2,7 @@ import { getContractAddress } from '../utils/contractAddress'
 import { useNetwork, useAccount } from 'wagmi'
 import { useHas1155Token } from '../hooks/has1155Token'
 import { useState, useEffect } from 'react'
-import { Box, Text, Badge } from '@chakra-ui/react'
+import { Box, Text, Badge, ListItem, UnorderedList } from '@chakra-ui/react'
 import { CheckIcon, NotAllowedIcon } from '@chakra-ui/icons'
 
 export const Erc1155Balance = () => {
@@ -15,9 +15,10 @@ export const Erc1155Balance = () => {
   })
   const tokenIds = [
     '66944570731711987735845020825364593806926065595037873500763412727051124211713',
-    '89863914919801430517959213592055400710389243750546056901808860984226390999047'
+    '89863914919801430517959213592055400710389243750546056901808860984226390999047',
+    '28583945730878618704816541677749661839032078384876779106450920615411361775622'
   ]
-  const tokenNames = ['Village', 'Music']
+  const tokenNames = ['Village', 'Music', 'kusa']
   let addresses = [address]
   while (addresses.length < tokenIds.length) addresses.push(address)
   const { balances, isError, error } = useHas1155Token(
@@ -48,14 +49,28 @@ export const Erc1155Balance = () => {
     )
   return (
     <Box mt="1em">
-      <Text>Owned tokens:</Text>
-      {ownedTokens.map((e, i) => {
-        return (
-          <Badge key={i} colorScheme="green">
-            <CheckIcon></CheckIcon> {e.name}
+      {ownedTokens.length > 0 ? (
+        <>
+          <Text>{ownedTokens.length} tokens owned:</Text>
+          <UnorderedList>
+            {ownedTokens.map((e, i) => {
+              return (
+                <ListItem key={i}>
+                  <Badge colorScheme="green" mr="0.5em">
+                    <CheckIcon></CheckIcon> {e.name}
+                  </Badge>
+                </ListItem>
+              )
+            })}
+          </UnorderedList>
+        </>
+      ) : (
+        <Text>
+          <Badge colorScheme="red">
+            <NotAllowedIcon></NotAllowedIcon> No tokens owned
           </Badge>
-        )
-      })}
+        </Text>
+      )}
     </Box>
   )
 }
